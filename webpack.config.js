74c8f9440b7dc941bbdb163c1959087a9bcc3a22
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-10-26 18:38:48 -0300

    adicionado webpack no projeto

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+require('dotenv').config();
+
+module.exports = {
+  mode: process.env.MODE,
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: './public',
+    port: 3333
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(css|scss)/,
+        use: [
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.svg?/,
+        use: {
+          loader: 'svg-inline-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', 'jsx']
+  }
+};
