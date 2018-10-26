96ce7d96d86da59532436a3cac0da6a28ff09fc2
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-10-26 20:28:44 -0300

    removido card do material-ui e adicionado css personalizado

--- src/Dashobard/Dashboard.jsx
+++ src/Dashobard/Dashboard.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import './index.scss';
 
 const Dashboard = () => (
-  <Card>
-    <CardContent>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio voluptatum
-      voluptate perspiciatis veritatis iste, accusamus ad soluta iusto error dignissimos
-       earum sed fugiat! Quos deleniti perferendis provident pariatur mollitia voluptates?
-        Tenetur, eligendi blanditiis! Aspernatur, repellendus natus! Neque deserunt necessitatibus
-         adipisci recusandae est, illum ex maiores! Iure laudantium quibusdam debitis! Nisi
-         doloribus nulla eum voluptas non similique mollitia expedita. Reprehenderit repellat
-         pariatur dolorum velit quisquam accusantium at odit, similique cupiditate praesentium.
-         In, eos doloribus temporibus ea ad adipisci laboriosam neque ipsum nesciunt debitis
-         cumque rem perferendis illo laudantium deleniti alias. Doloremque sit vitae
-         molestiae nisi iusto ex, provident saepe maiores officiis!
-    </CardContent>
-  </Card>
+  <div className="dashboard">
+    Removido opção do card do material-ui pois o mesmo esta quebrado dentro da
+    aplicação pois não esta achando a variavel <strong>@global</strong>, quando é feito o build da aplicação.
+  </div>
 );
 
 export default Dashboard;
