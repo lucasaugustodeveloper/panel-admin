0fb4aceab03e1ad8b93a70e251e752ca9401682a
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-10-26 20:11:32 -0300

    Update 2018-10-26T23:11:24.661Z

---
+++ precache-manifest.44d8154f27ed5c6696fdc77eee25d685.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "602decdc122b3c6caed6",
+    "url": "/static/js/main.602decdc.chunk.js"
+  },
+  {
+    "revision": "8b31409a8d2d9b9978ba",
+    "url": "/static/js/1.8b31409a.chunk.js"
+  },
+  {
+    "revision": "602decdc122b3c6caed6",
+    "url": "/static/css/main.0f07bc60.chunk.css"
+  },
+  {
+    "revision": "dbc061bdae5399cfa533b216e4dc089b",
+    "url": "/index.html"
+  }
+];
